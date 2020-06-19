01651a2373f6b4dae374dc324614966c5ef18344
Teffen Ellis <teffen@nirri.us>
2020-06-19 13:24:10 -0400

    Fix path.

--- linux.sh
+++ linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo pacman -S zsh
-sudo pacman -S diff-so-pretty
+sudo pacman -S diff-so-fancy
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
